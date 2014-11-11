2f2d66c8aa40e1b508891ef02d216564165c9c2b
Adrian Burgess <ade@mac.com>
2014-11-11 21:28:56 +1100

    firmware's updated (only works with 10.10)

--- lib/facter/mac_firmwarepass.rb
+++ lib/facter/mac_firmwarepass.rb
@@ -1,15 +1,21 @@
 #mac_firmwarepass.rb
 #requires setregproptool extracted from Firmware Password Utility installed to /usr/local/bin
+
+require 'facter'
 Facter.add(:mac_firmwarepass) do
   confine :kernel => "Darwin"
   setcode do
-    fwpass_status = Facter::Util::Resolution.exec("/usr/local/bin/setregproptool -c")
-    
-    if fwpass_status == "0"
-      "SET"
-    end
-    if fwpass_status == "1"
-      "NOT SET"
-    end
+    osver = Facter.value('macosx_productversion_major')
+    if osver == "10.10"
+    output = Facter::Util::Resolution.exec("/usr/sbin/firmwarepasswd -check")
+    fwpass_status = output.split("\n").first
+      if fwpass_status=="Password Enabled: No"
+        "false"
+      else
+        "true"
+      end
+    else
+      "Not supported"
   end
+end
 end
